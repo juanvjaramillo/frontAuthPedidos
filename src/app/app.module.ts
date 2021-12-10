47e9215ab505fb6b74aceaea3f80d31747ff8630
Juanse Villegas <juanvjaramillo@gmail.com>
2021-12-10 18:32:14 -0500

    carpeta plantillas se ingresa a modulos y se cambian las rutas en app.component, en app.module y app.routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
-import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
-import { InicioComponent } from './plantilla/inicio/inicio.component';
-import { ErrorComponent } from './plantilla/error/error.component';
+import { BarraNavegacionComponent } from './modulos/plantilla/barra-navegacion/barra-navegacion.component';
+import { PiePaginaComponent } from './modulos/plantilla/pie-pagina/pie-pagina.component';
+import { InicioComponent } from './modulos/plantilla/inicio/inicio.component';
+import { ErrorComponent } from './modulos/plantilla/error/error.component';
 import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
