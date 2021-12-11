492a955c9719867948f0fb32734c636e4313ab3f
Juanse Villegas <juanvjaramillo@gmail.com>
2021-12-11 01:09:37 -0500

    Authenticacion y CRUD de productos por medio de servicios al backend V0.2

--- src/app/modulos/administracion/productos/crear-producto/crear-producto.component.ts
+++ src/app/modulos/administracion/productos/crear-producto/crear-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ModeloProducto } from 'src/app/modelos/producto.modelo';
+import { ProductoService } from 'src/app/servicios/producto.service';
 
 @Component({
   selector: 'app-crear-producto',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearProductoComponent implements OnInit {
 
-  constructor() { }
+  fgValidador: FormGroup = this.fb.group({
+    'nombre': ['', [Validators.required]],
+    'precio': ['', [Validators.required]],
+    'imagen': ['', [Validators.required]]
+  })
+  constructor(private fb: FormBuilder, 
+    private servicioProducto: ProductoService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  GuardarProducto(){
+    let nombre = this.fgValidador.controls["nombre"].value;
+    let precio = parseInt(this.fgValidador.controls["precio"].value);
+    let imagen = this.fgValidador.controls["imagen"].value;
+    let producto = new ModeloProducto();
+    producto.nombre = nombre;
+    producto.precio = precio;
+    producto.imagen = imagen;
+    this.servicioProducto.CrearProducto(producto).subscribe((datos: ModeloProducto) => {
+      alert("Producto almacenado correctamente")
+      this.router.navigate(["/administracion/listar-productos"]);
+
+    }, (error: any) => {
+      alert("Error almacenando el producto")
+    })
+
   }
 
 }
